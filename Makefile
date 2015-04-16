a79c8ae9f77c2731a78b55b7ba4fe8c280b34123
Pablo Lins <pablo.lins@ideais.com.br>
2015-04-15 23:01:59 -0300

    Exercises before 'Recursive Parsers'

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 
 build:
-	ghc -o bin/scheme --make src/main.hs
+	ghc --make -o bin/scheme src/main.hs
 
 run:
-	./bin/scheme $(arg1)
+	./bin/scheme $(_)
 
 .PHONY: build run
--- src/main.hs
+++ src/main.hs
@@ -1,7 +1,66 @@
- module Main where
- import System.Environment
- 
- main :: IO ()
- main = do
-     args <- getArgs
-     putStrLn ("Hello, " ++ args !! 0)
+module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+main :: IO ()
+main = do
+	args <- getArgs
+	putStrLn (readExpr (args !! 0))
+
+
+data Value = Atom String
+           | List [Value]
+           | DottedList [Value] Value
+           | Number Integer
+           | String String
+           | Bool Bool 
+           deriving Show
+
+
+-- generaters a parser that checks if a
+-- character exists in !#$%&|*+-/:<=>?@^_~
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+
+-- generaters a parser that checks if next
+-- character is ' ' and skips it case true
+spaces :: Parser ()
+spaces = skipMany1 space
+
+
+parseString :: Parser Value
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return (String x)
+
+
+parseAtom :: Parser Value
+parseAtom = do 
+              first <- letter <|> symbol 
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first : rest :: [Char]
+              return $ case atom of 
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+
+
+parseNumber :: Parser Value
+parseNumber = liftM (Number . read) (many1 digit)
+
+
+parseExpr :: Parser Value
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+readExpr :: String -> String
+readExpr expr = case parsedExpr of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value: " ++ show val
+    where parsedExpr = parse parseExpr "scheme" expr
+  
